4ae530cebab2f149628ef58db1e185e46532c118
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-09 12:43:36 +0200

    users/user_id

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def index():
     return "Hello World!"
 
 
+@app.route('/users/<user_id>')
+def users(user_id):
+    return f"User id: {user_id}"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
