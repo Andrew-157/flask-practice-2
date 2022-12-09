9992a235b31402d944b51d4058add1e835ed64e9
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-09 13:23:49 +0200

    post, get

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+users_ids = []
 
 
 @app.route('/')
@@ -8,10 +10,18 @@ def index():
     return "Hello World!"
 
 
-@app.route('/users/<int:user_id>')
+@app.route('/users/<int:user_id>', methods=['GET', 'POST'])
 def users(user_id):
 
-    return f"User id: {user_id}"
+    if request.method == 'POST':
+        users_ids.append(user_id)
+        print(users_ids)
+        return f"User with id: {user_id} has been created"
+
+    if user_id not in users_ids:
+        return "User not found", 404
+
+    return f"User's id: {user_id}"
 
 
 if __name__ == '__main__':
