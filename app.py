b7d7e550a2c0d87bbc6b9f9063c279c77d5186d9
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-09 12:51:11 +0200

    custom validation

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ def index():
 
 @app.route('/users/<user_id>')
 def users(user_id):
+    try:
+        user_id = int(user_id)
+    except ValueError:
+        return "Validation error. User id should be integer.", 400
+
     return f"User id: {user_id}"
 
 
